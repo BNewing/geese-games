c724e68614cdc5ad86804673d20e00b03965854a
BNewing <bnewing92@hotmail.co.uk>
2018-08-12 18:35:39 +0100

    Adds quiz question data

---
+++ src/components/quiz/quizQuestions.js
@@ -0,0 +1,182 @@
+var quizQuestions = [
+
+	{
+		question: "What type of goose is this?",
+		image: "https://thumb9.shutterstock.com/display_pic_with_logo/873577/555906772/stock-photo-greylag-goose-open-the-wings-on-the-water-surface-555906772.jpg",
+		correctAnswer: "Greylag goose",
+		answers: [
+			{
+				type: "Brent goose"
+			},
+			{
+				type: "Canada goose"
+			},
+			{
+				type: "Greylag goose"
+			},
+			{
+				type: "Bean goose" 
+			}
+
+		]
+	},
+	{
+		question: "What type of goose is this?",
+		image: "https://thumb7.shutterstock.com/display_pic_with_logo/517861/176951771/stock-photo-greater-white-fronted-goose-176951771.jpg",
+		correctAnswer: "Greater white frontend goose",
+		answers: [
+			{
+				type: "Greylag goose"
+			},
+			{
+				type: "Greater white frontend goose"
+			},
+			{
+				type: "Chinese goose"
+			},
+			{
+				type: "Pink footed goose" 
+			}
+
+		]
+	},
+	{
+		question: "What type of goose is this?",
+		image: "https://thumb9.shutterstock.com/display_pic_with_logo/2270711/187360778/stock-photo-bean-goose-187360778.jpg",
+		correctAnswer: "Bean goose",
+		answers: [
+			{
+				type: "Pink footed goose"
+			},
+			{
+				type: "Chinese goose"
+			},
+			{
+				type: "Bean goose"
+			},
+			{
+				type: "Brent goose" 
+			}
+		]
+	},
+	{
+		question: "What type of goose is this?",
+		image: "https://thumb1.shutterstock.com/display_pic_with_logo/518803/271545113/stock-photo-chinese-goose-closeup-in-a-poultry-farm-271545113.jpg",
+		correctAnswer: "Chinese goose",
+		answers: [
+			{
+				type: "Chinese goose"
+			},
+			{
+				type: "Canada goose"
+			},
+			{
+				type: "Bean goose"
+			},
+			{
+				type: "Greylag goose" 
+			}
+
+		]
+	},	
+	{
+		question: "What type of goose is this?",
+		image: "https://thumb7.shutterstock.com/display_pic_with_logo/1699051/178187834/stock-photo-portrait-of-a-male-dark-bellied-brent-goose-the-netherlands-178187834.jpg",
+		correctAnswer: "Brent goose",
+		answers: [
+			{
+				type: "Egyptian goose"
+			},
+			{
+				type: "Greater white frontend goose"
+			},
+			{
+				type: "Bean goose"
+			},
+			{
+				type: "Brent goose" 
+			}
+
+		]
+	},
+	{
+		question: "What type of goose is this?",
+		image: "https://thumb9.shutterstock.com/display_pic_with_logo/161300779/461700892/stock-photo-barnacle-goose-branta-leucopsis-barents-sea-coast-timan-tundra-russia-461700892.jpg",
+		correctAnswer: "Barnacle goose",
+		answers: [
+			{
+				type: "Egyptian goose"
+			},
+			{
+				type: "Barnacle goose"
+			},
+			{
+				type: "Brent goose"
+			},
+			{
+				type: "Canada goose" 
+			}
+
+		]
+	},
+	{
+		question: "What type of goose is this?",
+		image: "https://thumb1.shutterstock.com/display_pic_with_logo/11724/634786370/stock-photo-canada-goose-family-at-richmond-bc-canada-634786370.jpg",
+		correctAnswer: "Canada goose",
+		answers: [
+			{
+				type: "Canada goose"
+			},
+			{
+				type: "Greylag goose"
+			},
+			{
+				type: "Barnacle goose"
+			},
+			{
+				type: "Chinese goose" 
+			}
+
+		]
+	},
+	{
+		question: "What type of goose is this?",
+		image: "https://thumb9.shutterstock.com/display_pic_with_logo/770932/625318562/stock-photo-egyptian-goose-alopochen-aegyptiacus-625318562.jpg",
+		correctAnswer: "Egyptian goose",
+		answers: [
+			{
+				type: "Pink footed goose"
+			},
+			{
+				type: "Egyptian goose"
+			},
+			{
+				type: "Greater white frontend goose"
+			},
+			{
+				type: "Bean goose" 
+			}
+
+		]
+	},
+	{
+		question: "What type of goose is this?",
+		image: "https://thumb1.shutterstock.com/display_pic_with_logo/1149083/152223113/stock-photo-pink-footed-goose-anser-brachyrhynchus-scotland-spring-152223113.jpg",
+		correctAnswer: "Pink footed goose",
+		answers: [
+			{
+				type: "Chinese goose"
+			},
+			{
+				type: "Greater white frontend goose"
+			},
+			{
+				type: "Bean goose"
+			},
+			{
+				type: "Pink footed goose" 
+			}
+
+		]
+	}
+]
