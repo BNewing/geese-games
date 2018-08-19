4f046ae594f212aee0fb7a341892ca1114cd8fe4
BNewing <bnewing92@hotmail.co.uk>
2018-08-19 13:41:31 +0100

    Removes unneccessary references to tidy console warnings up

--- src/components/quiz/quiz.js
+++ src/components/quiz/quiz.js
@@ -8,15 +8,10 @@ import QuestionCounter from './questionCounter';
 import AnswerOption from './answerOption';
 import quizQuestions from './quizQuestions';
 
-const Text = styled.p `
-  text-align: center;
-  padding-top: 2rem;
-`
-
 const Image = styled.img `
-  padding: 5rem 0;
   margin: auto;
   display: block;
+  border-radius: 50%;
 `
 
 const PageWrapper = styled.div `
@@ -33,45 +28,51 @@ export default class GeeseInfo extends Component {
        question: '',
        image: '',
        answerOptions: [],
-       answer: '',
-       correctAnswers: '',
-       result: '',
-       selectedAnswer: ''
-      };
+       correctAnswer: '',
+       selectedAnswer: '',
+       numberCorrectAnswers: 0,
+    };
 
       this.loadNextQuestion = this.loadNextQuestion.bind(this);
       this.checkAnswer = this.checkAnswer.bind(this);
+      this.optionSelected = this.optionSelected.bind(this);
     };
 
     componentWillMount(){
       this.setState({
         question: quizQuestions[this.state.counter].question,
         image: quizQuestions[this.state.counter].image,
+        correctAnswer: quizQuestions[this.state.counter].correctAnswer,
         answerOptions: quizQuestions[this.state.counter].answers
       });
     }
 
     checkAnswer(){
+      if (this.state.selectedAnswer === this.state.correctAnswer) {
+        this.setState({
+          numberCorrectAnswers: this.state.numberCorrectAnswers +1
+        })
+      }
       this.loadNextQuestion();
+      console.log(this.state);
     }
 
     loadNextQuestion() {
-
       this.setState({ 
         counter: this.state.counter + 1,  
         questionId: this.state.questionId +1,
         image: quizQuestions[this.state.counter].image,
         question: quizQuestions[this.state.counter].question,
+        correctAnswer: quizQuestions[this.state.counter].correctAnswer,
         answerOptions: quizQuestions[this.state.counter].answers,
+        selectedAnswer: ''
       });
     }
     
-    optionSelected(e) {
-      
+    optionSelected(e) { 
       this.setState({
         selectedAnswer: e
       })
-      console.log(this.state);
     }
 
     render() {
@@ -79,13 +80,12 @@ export default class GeeseInfo extends Component {
       return (
         <PageWrapper>
         	<Nav />	
-            
             <QuestionCounter 
               counter={this.state.questionId} 
               total={quizQuestions.length} />
             <Question 
               question={this.state.question}/>
-            <img src={this.state.image} alt="goose" />
+            <Image src={this.state.image} alt="goose" />
             <ul>
               {this.state.answerOptions.map((item,i) => <AnswerOption key={i} answer={item} handleChange={e=>this.optionSelected(e)}/>)}
             </ul>
