d12df3e3e62c4c779855db5783eca4f8ef5bc9a6
BNewing <bnewing92@hotmail.co.uk>
2018-08-19 17:12:16 +0100

    Alert to show answer and tidying up quiz end page

--- src/components/quiz/quiz.js
+++ src/components/quiz/quiz.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Nav from '../common/nav';
 import Footer from '../common/footer';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 import Question from './question';
 import QuestionCounter from './questionCounter';
@@ -31,6 +32,11 @@ const List = styled.ul `
   padding: 0;
 `
 
+const StyledLink = styled(Link) `
+  text-decoration: none;
+  color: black;
+`
+
 export default class GeeseInfo extends Component {
   constructor(props) {
     super(props);
@@ -43,6 +49,7 @@ export default class GeeseInfo extends Component {
        correctAnswer: '',
        selectedAnswer: '',
        numberCorrectAnswers: 0,
+       quizCompleted: false
     };
       this.loadNextQuestion = this.loadNextQuestion.bind(this);
       this.checkAnswer = this.checkAnswer.bind(this);
@@ -65,17 +72,17 @@ export default class GeeseInfo extends Component {
           numberCorrectAnswers: this.state.numberCorrectAnswers +1
         })
       }
-      setTimeout(alert("Correct answer: " + this.state.correctAnswer), 3000);
+      alert("You chose " + this.state.selectedAnswer + ", and the correct answer: " + this.state.correctAnswer);
       
-      if (!this.state.questionId < quizQuestions.length) {
+      if (this.state.questionId < quizQuestions.length) {
           console.log(this.state.questionId, quizQuestions.length);
           this.loadNextQuestion();
-          //this if statement doesn't ever trigger false
       } else {
         this.setState ({
           image: '',
           question: '',
-          answerOptions: '',
+          answerOptions: [],
+          quizCompleted: true
         })
         
       }
@@ -106,17 +113,19 @@ export default class GeeseInfo extends Component {
       return (
         <PageWrapper>
         	<Nav />	
-            {this.state.questionId > quizQuestions.length ? (<p>Quiz completed! You got {this.state.numberCorrectAnswers} out of {this.state.questionId} questions right.</p>) : (<p>Quiz not completed</p>)}
+            {this.state.quizCompleted ? <p>You got {this.state.numberCorrectAnswers} out of {this.state.questionId} questions right.</p> : (
             <QuestionCounter 
               counter={this.state.questionId} 
               total={quizQuestions.length} />
+              )}
             <Question 
               question={this.state.question}/>
-            <Image src={this.state.image} alt="goose" />
+            {this.state.quizCompleted ? (<p>Alllll the learning about geese</p>) : (<Image src={this.state.image} alt="goose" />)}
             <List>
               {this.state.answerOptions.map((item,i) => <AnswerOption key={i} answer={item} handleChange={e=>this.optionSelected(e)}/>)}
             </List>
-            <Button onClick={this.checkAnswer}>Submit answer</Button>
+            {this.state.quizCompleted ? (<Button><StyledLink to="/geese-info">Go back to the geese info page</StyledLink></Button> ): (
+            <Button onClick={this.checkAnswer}>Submit answer</Button>)}
           <Footer />
         </PageWrapper>
     );
