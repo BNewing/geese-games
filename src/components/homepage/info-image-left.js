23e87540fe50d6c0f8c47c7d499edbd6429817e3
BNewing <bnewing92@hotmail.co.uk>
2019-09-28 23:01:23 +0100

    Update homepage text to clarify brent goose photo isn't mine

--- src/components/homepage/info-image-left.js
+++ src/components/homepage/info-image-left.js
@@ -29,7 +29,7 @@ export default class InfoImageLeft extends Component {
           <Image src={HandFeeding} alt="Cute goose" />
           <h2>What is this website?</h2>
           <p>This site is an educational resource with a quiz - check out different types of geese on the 'Geese Info' and then test out what you've learnt on the 'Quiz' page!</p>
-          <p>Technology-wise, this is a web app that I've built with React and designed myself. It's a fun little side project where I test out new technologies like styled-components, and practise bits of JavaScript that I'd like to get better at. The photos of geese are all ones that I've taken myself, and the info about the different types of geese is from Wikipedia and the RSPB website.</p> 
+          <p>Technology-wise, this is a web app that I've built with React and designed myself. It's a fun little side project where I test out new technologies like styled-components, and practise bits of JavaScript that I'd like to get better at. The photos of geese are all ones that I've taken myself (aside from the Brent goose, as I haven't seen one of those yet), and the info about the different types of geese is from Wikipedia and the RSPB website.</p> 
       </ComponentWrapper>
     );
   }
